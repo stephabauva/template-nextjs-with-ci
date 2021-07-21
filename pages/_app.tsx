95d4f56fc6ccddc899cd8d63422be603158a0e52
sb <stephane.baudry.vanaa@gmail.com>
2021-07-21 15:46:15 +0200

    modif type _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable require-jsdoc */
 /* eslint-disable react/react-in-jsx-scope */
+import { AppProps } from "next/app"; //
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
